eb38fa5c05742aca13571e7cafb97cdb9d9605f9
Mythic <mythicmaniac@gmail.com>
2019-05-18 20:04:42 +0300

    Add better data volume support

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
-if ! [ -L /home/teamspeak3/teamspeak3-server_linux_amd64/ts3server.sqlitedb ]; then
-    ln -s /data/ts3server.sqlitedb /home/teamspeak3/teamspeak3-server_linux_amd64/ts3server.sqlitedb
+volume=/data/
+tsdir=/home/teamspeak3/teamspeak3-server_linux_amd64/
+if ! [ -L ${tsdir}ts3server.sqlitedb ]; then
+    ln -s ${volume}ts3server.sqlitedb ${tsdir}ts3server.sqlitedb
+fi
+if ! [ -L ${tsdir}files ]; then
+    ln -s ${volume}files ${tsdir}files
+fi
+if ! [ -L ${tsdir}query_ip_whitelist.txt ]; then
+    ln -s ${volume}query_ip_whitelist.txt ${tsdir}query_ip_whitelist.txt
+fi
+if ! [ -L ${tsdir}query_ip_blacklist.txt ]; then
+    ln -s ${volume}query_ip_blacklist.txt ${tsdir}query_ip_blacklist.txt
+fi
+if ! [ -L ${tsdir}licensekey.dat ]; then
+    ln -s ${volume}licensekey.dat ${tsdir}licensekey.dat
 fi
 sh /home/teamspeak3/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh inifilte=ts3server.ini
